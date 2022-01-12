4a0296f8335d0e139c74e17af55623bab7c00b1a
Sajjad <65295591+Sajjad-Mi@users.noreply.github.com>
2022-01-13 01:02:40 +0330

    make server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const mongoose = require('mongoose');
+
+
+require('dotenv').config();
+
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true,  useUnifiedTopology: true})
+.then(()=>{
+  server.listen(PORT);
+})
