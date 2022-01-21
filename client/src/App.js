3c51bc9853f443f51acc24b388626120480b7de2
sajjad <65295591+Sajjad-Mi@users.noreply.github.com>
2022-01-21 23:32:47 +0330

    add auth routes in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+        <div className="app">
+          <Routes>
+            <Route exact path="/login" element={<Login/>} />
+            <Route exact path="/signup" element={<Signup/>} />
+
+          </Routes>
+        </div>
+    </Router>
   );
 }
 
